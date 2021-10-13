d25468fe1d379bb9d5341916b13803f0f14b8490
Evgeny Eroshin <eee333@yandex.ru>
2021-10-13 15:41:33 +0300

    Create rout index

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 # Курс 2. Курсовая. свой Instagram с едой, котами и еще раз едой!
 
+from flask import Flask, request, render_template
+from funcs import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    posts = get_posts()
+    return render_template('index.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
--- funcs.py
+++ funcs.py
@@ -1,5 +1,7 @@
 # All function for project
 
+import json
+
 
 def get_posts():
     with open('data/data.json', encoding="utf-8") as f:
