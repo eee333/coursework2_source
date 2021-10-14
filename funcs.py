6fe991c96e18de84849ba2b996c8f854420d48f8
Evgeny Eroshin <eee333@yandex.ru>
2021-10-14 14:09:48 +0300

    Add function get_comments

--- funcs.py
+++ funcs.py
@@ -8,3 +8,15 @@ def get_posts():
         posts = json.load(f)
         return posts
 
+
+def get_comments(post_id):
+    comment_list = []
+    with open('data/comments.json', encoding="utf-8") as f:
+        comments = json.load(f)
+    for comment in comments:
+        if comment['post_id'] == post_id:
+            comment_list.append(comment)
+    return comment_list
+
+
+
