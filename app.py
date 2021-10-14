fdcd6a2c8a816ab9fa7aaf333a9d09d8e46fdd74
Evgeny Eroshin <eee333@yandex.ru>
2021-10-14 15:40:31 +0300

    Bugfix for search without args

--- app.py
+++ app.py
@@ -25,13 +25,15 @@ def post_page(post_id):
 @app.route('/search/')
 def search_page():
     search_txt = request.args.get('s')
+    posts_found = []
     if search_txt:
-        posts_found = []
         posts = get_posts()
         for post in posts:
             if search_txt.lower() in post['content'].lower():
                 posts_found.append(post)
-        return render_template('search.html', posts=posts_found, search_txt=search_txt)
+    else:
+        search_txt = ''
+    return render_template('search.html', posts=posts_found, search_txt=search_txt)
 
 
 if __name__ == '__main__':
